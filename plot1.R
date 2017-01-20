e9577b1f1d9b65c7efeb4b3c9ca5214b5d21aed5
Erin Horiuchi <erin.m.horiuchi@gmail.com>
2017-01-19 20:27:15 -0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -17,6 +17,6 @@ powerConsumptionSub$Global_active_power <- as.numeric(powerConsumptionSub$Global
 
 ## Create plot1
 
+png(filename="plot1.png", height=480, width=480, bg="transparent")
 hist(powerConsumptionSub$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
-dev.copy(png, file="plot1.png", height=480, width=480)
 dev.off()
